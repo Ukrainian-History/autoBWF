d5eb4df861a305afcc94e364f7ec7c24b32a6298
Michael Andrec <MAndrecPhD@gmail.com>
2019-12-16 21:45:06 -0500

    Add -o option to bwf2csv

--- autoBWF/bwf2csv.py
+++ autoBWF/bwf2csv.py
@@ -9,6 +9,7 @@ from autoBWF.BWFfileIO import *
 def main():
     parser = argparse.ArgumentParser(
         description='Extract metadata from BWF and create PBCore XML, possibly incorporating into existing OHMS XML')
+    parser.add_argument('-o', dest="outfile", help="CSV output file")
     parser.add_argument('infile', nargs="+", help="WAV file(s)")
     args = parser.parse_args()
 
@@ -16,14 +17,22 @@ def main():
                      "ISRC", "xmp_description", "interviewer", "interviewee", "host", "speaker",
                      "performer", "topics", "names", "events", "places", "owner", "ICOP"]
 
-    output = csv.DictWriter(sys.stdout, output_fields)
+    if args.outfile is not None:
+        output = csv.DictWriter(open(args.outfile, 'w'), output_fields)
+    else:
+        output = csv.DictWriter(sys.stdout, output_fields)
+
     output.writeheader()
+    rows = []
 
     for infile in args.infile:
         metadata = get_bwf_core(True, infile)
         metadata.update(get_bwf_tech(True, infile))
         metadata.update(get_xmp(infile, ["bwfmetaedit", "--specialchars", "--accept-nopadding"]))
-        output.writerow({k: metadata[k] for k in output_fields if k in metadata})
+        rows.append({k: metadata[k] for k in output_fields})
+
+    for row in rows:
+        output.writerow(row)
 
 
 if __name__ == '__main__':
