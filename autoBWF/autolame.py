41b4ce7ccbbb1cb23d8b7939eec5d5cc1f2654a5
Michael Andrec <MAndrecPhD@gmail.com>
2020-02-17 13:57:34 -0500

    Refactor autolame to allow calling from autoBWF export

--- autoBWF/autolame.py
+++ autoBWF/autolame.py
@@ -4,6 +4,36 @@ import re
 from autoBWF.BWFfileIO import *
 
 
+# There is no easy way to write XMP to MP3 without the python-xmp-toolkit and exempi (which was eliminated in v3.1).
+# Therefore, the extra tags that were encoded in the earlier versions are now also eliminated.
+
+
+def construct_command(infile, outfile, metadata, vbr_level):
+    command = ["lame", "-V", vbr_level, "--vbr-new"]
+
+    if metadata["INAM"] != "":
+        command.extend(['--tv', 'TIT2={}'.format(metadata["INAM"])])
+    if metadata["IARL"] != "":
+        command.extend(['--tv', "TOWN={}".format(metadata["IARL"])])
+    if metadata["ICOP"] != "":
+        command.extend(['--tv', "TCOP={}".format(metadata["ICOP"])])
+    if metadata["ISRC"] != "":
+        command.extend(['--tv', "TIT1={}".format(metadata["ISRC"])])
+
+    if metadata["Description"] != "":
+        m = re.search(r'File content: +(.+?);', metadata["Description"])
+        if m:
+            command.extend(['--tv', "TIT3={}".format(m.group(1))])
+
+    if metadata["ICRD"] != "":
+        command.extend(["--ty", metadata["ICRD"]])
+
+    command.extend(['--tv', "TLAN={}".format(metadata["language"])])
+    command.extend(["--id3v2-only", infile, outfile])
+
+    return command
+
+
 def main():
     parser = argparse.ArgumentParser(
         description='Use lame to create mp3 file from BWF, using BWF metadata to populate IDv2 tags')
@@ -16,39 +46,14 @@ def main():
         sys.exit("Can only have one input file if output file is specified.")
 
     for infile in args.infile:
-        command = ["lame", "-V", str(args.vbr_level), "--vbr-new"]
-
         if args.outfile is None:
             outfile = infile.rsplit('.', 1)[0] + '.mp3'
         else:
             outfile = args.outfile
 
-        bwf_metadata = get_bwf_core(True, infile)
-        xmp_metadata = get_xmp(infile, ["bwfmetaedit", "--specialchars", "--accept-nopadding"])
-
-        if bwf_metadata["INAM"] != "":
-            command.extend(['--tv', 'TIT2={}'.format(bwf_metadata["INAM"])])
-        if bwf_metadata["IARL"] != "":
-            command.extend(['--tv', "TOWN={}".format(bwf_metadata["IARL"])])
-        if bwf_metadata["ICOP"] != "":
-            command.extend(['--tv', "TCOP={}".format(bwf_metadata["ICOP"])])
-        if bwf_metadata["ISRC"] != "":
-            command.extend(['--tv', "TIT1={}".format(bwf_metadata["ISRC"])])
-
-        if bwf_metadata["Description"] != "":
-            m = re.search(r'File content: +(.+?);', bwf_metadata["Description"])
-            if m:
-                command.extend(['--tv', "TIT3={}".format(m.group(1))])
-
-        if bwf_metadata["ICRD"] != "":
-            command.extend(["--ty", bwf_metadata["ICRD"]])
-
-        command.extend(['--tv', "TLAN={}".format(xmp_metadata["language"])])
-        command.extend(["--id3v2-only", infile, outfile])
-        subprocess.call(command)
-
-    # no easy way to write XMP to MP3 without the python-xmp-toolkit and exempi (which has been eliminated), so
-    # we're eliminating the extra tags that were encoded in the earlier versions
+        metadata = get_bwf_core(True, infile)
+        metadata.update(get_xmp(infile, ["bwfmetaedit", "--specialchars", "--accept-nopadding"]))
+        subprocess.call(construct_command(infile, outfile, metadata, str(args.vbr_level)))
 
 
 if __name__ == '__main__':
